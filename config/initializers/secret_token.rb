5f7350e902046a62047100cba5ddb69318087003
Oge Nnadi <oge@nnadi.org>
2013-01-10 09:05:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailClient::Application.config.secret_token = '2dffe71e40c029fe27f657a9fbb6fee7bef937fa0786407b1b022b5abb5cf49cae0413551b70243bbc446fcfe249f1177a07dc7b6635675fe1850a47157702a9'
